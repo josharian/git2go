7d45f55a4c3f898f16387a5a71222f98b4b7a6cf
Josh Bleecher Snyder <josharian@gmail.com>
2019-12-04 13:11:47 -0800

    require v28 instead of v27

--- git_static.go
+++ git_static.go
@@ -9,8 +9,8 @@ package git
 #cgo !windows pkg-config: --static ${SRCDIR}/libgit2/build/libgit2.pc
 #include <git2.h>
 
-#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 27
-# error "Invalid libgit2 version; this git2go supports libgit2 v0.27"
+#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 28
+# error "Invalid libgit2 version; this git2go supports libgit2 v0.28"
 #endif
 
 */
